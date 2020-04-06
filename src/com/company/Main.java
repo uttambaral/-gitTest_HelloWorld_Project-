000959d79cbe68acd57788630fdd1de14cbfdd7d
uttambaral <uttambaral@gmail.com>
2020-04-06 16:23:34 -0500

    github.com added..

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("hello world ! from github.com..");
         System.out.print("Sandesh Lamsal");
-	
+
+        //April62020
+        int[] intArray = {1, 2, 3, 4, 5};
+        for (int a : intArray) {
+            System.out.println(a);
+        }
     }
 }
+
